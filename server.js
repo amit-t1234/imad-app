5d4af6028f0dd42657a8d87f05c81448d1526233
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-08-25 14:51:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article1',function(req,res){
-   res.send("this is article one"); 
+app.get('/article1',function (req,res) {
+   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
