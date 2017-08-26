dcc1d4e48b4cec84423266e3fc99b426112bfae1
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-08-26 13:11:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //changing the text of main page.
 var element = document.getElementById('main.text');
 element.innerHTML = 'I am amit and this is my web page!!';
+
+//changing the image style
+var img = document.getElementById('img');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+};
