a3384750970155cfdf4a4e4ecd93da8ebfdec8a8
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-08-26 15:30:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,6 +17,23 @@ button.onclick = function(){
       }
       //else do nothing.
   };
-  request.open('GET','http://amitthakurashwani.imad.hasura-app.io/counter',true)
+  request.open('GET','http://amitthakurashwani.imad.hasura-app.io/counter',true);
   request.send(null);
+};
+
+//submitname
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  //make the request to the server and send the name
+  
+  //dispaly the list of name 
+  var names = ['name1','name2','name3','name4'];
+  var list = '';
+  for(i=0;i<names.length;i++){
+      list = list + '<li>'+names[i]+'</li>'
+  }
+  var ul = document.getElementById('inputList');
+  ul.innerHTML = list;
 };
