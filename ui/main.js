dc8c71a684645f2d4a5e5778a8149c3f733a4748
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-08-26 13:16:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,15 @@ element.innerHTML = 'I am amit and this is my web page!!';
 
 //changing the image style
 var img = document.getElementById('img');
+var count = 0;
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+    if(count === 0){
+        img.style.marginLeft = '100px';
+        count = 1;
+    }
+    else{
+        img.style.marginRight  = '100px';
+        count = 0;
+    }
+    
 };
