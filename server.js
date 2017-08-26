80b94f34c9e13fc6989049741c328ab0b6482762
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-08-26 14:30:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,9 +116,8 @@ app.get('/:articleName',function (req,res) {
 });
 
 var counter = 0;
-
-app.get('/counter',function (req,res){
-    counter = counter +1;
+app.get('/counter', function (req,res){
+    counter = counter + 1;
     res.send(counter.toString());
 });
 
