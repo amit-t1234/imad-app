c0edfc2e9977046d2124a30210b207a7b7e07ba3
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-08-26 14:52:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,22 @@
 //counter increase button
-var counter = 0;
 var button = document.getElementById('counter');
 
 
 button.onclick = function(){
   //make a request to the capture endpoint
-  
+  var request = new XMLHttpRequest();
   //capture the request
-  
-  //display the increase in pointer.
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  request.onreadystatechange = function(){
+      if (request.readyState === XMLHttpRequest.DONE){
+          //do some thing
+          if(request.status === 200){
+                var counter = request.responseText; 
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+          }
+      }
+      //else do nothing.
+  };
+  request.open('GET','http://amitthakurashwani.imad.hasura-app.io/counter',true)
+  request.send(null);
 };
