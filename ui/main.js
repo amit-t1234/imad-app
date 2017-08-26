ea23658cac36627028e6381ae101d9e0ea27873c
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-08-26 14:16:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,15 @@
-console.log('Loaded!');
+//counter increase button
+var counter = 0;
+var button = document.getElementById('counter');
 
-//changing the text of main page.
-var element = document.getElementById('main.text');
-element.innerHTML = 'I am amit and this is my web page!!';
 
-//changing the image style
-var img = document.getElementById('img');
-var marginLeft = 0;
-function moveRight (){
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
+button.onclick = function(){
+  //make a request to the capture endpoint
+  
+  //capture the request
+  
+  //display the increase in pointer.
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
