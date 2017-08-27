bf7b6bcd7eb5bd31d32df6efd3f34296cc7dcae3
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-08-27 23:00:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -153,8 +153,6 @@ app.get('/article/:articleName',function (req,res) {
    pool.query("SELECT * FROM article WHERE title ='" + req.param.articleName + "'", function(err,result){
        if(err){
            res.status(500).send(err.toString());
-       }else if(result.rows.length === 0){
-           res.status(404).send('data not found');
        }
        else{
            var articleData = result.rows[0];
