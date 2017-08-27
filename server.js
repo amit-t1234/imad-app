fcf17a038ea62906773b177b6e99e3de5fc28a4c
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-08-27 23:02:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ function createTemplate(data){
 
 
 app.get('/article/:articleName',function (req,res) {
-   pool.query("SELECT * FROM article WHERE title ='" + req.param.articleName + "'", function(err,result){
+   pool.query("SELECT * FROM article WHERE title = '" + req.param.articleName + "'", function(err,result){
        if(err){
            res.status(500).send(err.toString());
        }else if(result.rows.length === 0){
