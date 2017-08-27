6f917a52b93326a61c04f41636585acfdc8f5dac
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-08-27 22:09:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,33 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var pool = require('pg').pool;
+var config = {
+  user: 'amitthakurashwani',
+  database: 'amitthakurashwani',
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  password: process.env.DB_PASSWORD,
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new pool(config);
+app.get('/test-db', function(req, res){
+   //make a request to database
+   
+   //display the result
+   pool.query('SELECT FROM * test',function (err, result){
+      if(err){
+          res.status(500).send(err.toString());
+      } 
+      else{
+          res.send(JSON.stringify(result));
+      }
+   });
+});
 
 var counter = 0;
 app.get('/counter', function (req, res){
