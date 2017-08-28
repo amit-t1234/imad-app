87017b02498c3c4605629d0e45b2e293735eb997
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-08-28 13:59:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,8 +154,14 @@ app.get('/articles/:articleName',function (req,res) {
        if(err){
            res.status(500).send(err.toString());
        }else{
-           res.send(JSON.stringify(result));
-        }
+       
+           if(result.rows.length === 0){
+               res.status(404).send('Article not found');
+           }else{
+               var articleData = result.rows[0];
+               res.send(createTemplate(articleData));
+           }
+       }
    });
 });
 
