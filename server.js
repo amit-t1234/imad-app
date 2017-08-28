c84f7668025e95f093eec09a9be9deccd3c1d3a6
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-08-28 13:58:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ app.get('/articles/:articleName',function (req,res) {
        if(err){
            res.status(500).send(err.toString());
        }else{
-           res.send(JSON.stringify(result).rows);
+           res.send(JSON.stringify(result));
         }
    });
 });
