30637bdd13078166f28318167629bd8efaa43398
amit-t1234 <30929264+amit-t1234@users.noreply.github.com>
2017-09-02 13:23:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@
             }
             else{
                 var dbString = result.rows[0].password;
-                var salt = dbString.split('$')[2];
+                var salt = dbString.split($)[2];
                 var hashedPassword = hash(password, salt);
                 
                 if(hashedPassword === dbString){
